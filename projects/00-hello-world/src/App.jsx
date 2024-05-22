0ecd63aa978fb1303f3b9f6468405cc5be4ac51a
Pau Carrera <poksy2002@gmail.com>
2024-05-23 01:44:33 +0200

    feat: initial commit
    Create the initial project structure for a simple React application.

---
+++ projects/00-hello-world/src/App.jsx
@@ -0,0 +1,18 @@
+export function App () {
+  return (
+    <article>
+      <header>
+        <img src="https://unavatar.io/github/poksyy" alt="Poksyy avatar" />
+        <div>
+          <strong>Pau Carrera</strong>
+          <span>@poksyy</span>
+        </div>
+      </header>
+      <aside>
+        <button>
+          Follow
+        </button>
+      </aside>
+    </article>
+  )
+}
